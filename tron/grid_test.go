8127254a90ab912a342849e9feacc0a3f7b461a8
Sam Naser <sammnaser@gmail.com>
2021-05-29 12:41:23 -0700

    Refactor test

--- tron/grid_test.go
+++ tron/grid_test.go
@@ -9,9 +9,9 @@ func TestGridInit(t *testing.T) {
 	g := newGrid(3, 3)
 	g.initialize()
 	tests := []gridTestCase{
-		{x: 0, y: 0, expected: 1},
-		{x: 1, y: 1, expected: 0},
-		{x: 2, y: 2, expected: 1},
+		{x: 0, y: 0, expected: WALL},
+		{x: 1, y: 1, expected: EMPTY},
+		{x: 2, y: 2, expected: WALL},
 	}
 	gridTestCases(tests).run(t, g)
 }
@@ -22,14 +22,18 @@ type gridTestCase struct {
 	expected uint8
 }
 
+func (g gridTestCase) run(t *testing.T, grid *grid) {
+	got := grid.get(g.x, g.y)
+	if got != uint8(g.expected) {
+		t.Errorf("wanted %d at (%d, %d), got %d", g.expected, g.x, g.y, got)
+		fmt.Println(grid.String())
+	}
+}
+
 type gridTestCases []gridTestCase
 
 func (g gridTestCases) run(t *testing.T, grid *grid) {
 	for _, c := range g {
-		got := grid.get(c.x, c.y)
-		if got != uint8(c.expected) {
-			t.Errorf("wanted %d at (%d, %d), got %d", c.expected, c.x, c.y, got)
-			fmt.Println(grid.String())
-		}
+		c.run(t, grid)
 	}
 }
